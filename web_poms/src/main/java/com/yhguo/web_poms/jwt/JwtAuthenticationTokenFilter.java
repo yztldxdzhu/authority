7b58fc5fa656525d4bf8851c0bce8ffb57bc6813
yhguo <562806487@qq.com>
2019-06-11 21:45:30 +0800

    test

--- web_poms/src/main/java/com/yhguo/web_poms/jwt/JwtAuthenticationTokenFilter.java
+++ web_poms/src/main/java/com/yhguo/web_poms/jwt/JwtAuthenticationTokenFilter.java
@@ -44,6 +44,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException {
+        System.out.println(jwtTokenUtil.getHeader());
         String authHeader = request.getHeader(jwtTokenUtil.getHeader());
         try {
             if (authHeader != null && StringUtils.isNotEmpty(authHeader)) {
